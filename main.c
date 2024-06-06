c4eb7fe7b023093abc75837727964219e98e6cd4
Regan-Koopmans <regankoop20@gmail.com>
2024-06-06 18:01:53 +0200

    Parse tags

--- main.c
+++ main.c
@@ -4,9 +4,6 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
-
 /*
  * The tiff header appears right at the start of the file.
  * The header gives us a byte order for the file, and a
@@ -14,15 +11,15 @@
  * tiff file. An offset tells us where the first TifDirectory is
  */
 struct TifHeader {
-  short byte_order;
-  short magic_number;
-  int ifd_offset;
+	short byte_order;
+	short magic_number;
+	int ifd_offset;
 };
 
 /*
- */
+*/
 struct TifDirectory {
-  short entry_count;
+	short entry_count;
 };
 
 /*
@@ -31,75 +28,99 @@ struct TifDirectory {
  * of the layer, or the compression scheme used
  */
 struct TifTag {
-  short tag_id;    /* The tag identifier  */
-  short data_type; /* The scalar type of the data items  */
-  int data_count;  /* The number of items in the tag data  */
-  int data_offset; /* The byte offset to the data items  */
+	short tag_id;    /* The tag identifier  */
+	short data_type; /* The scalar type of the data items  */
+	int data_count;  /* The number of items in the tag data  */
+	int data_offset; /* The byte offset to the data items  */
 };
 
-int render() {
-  SDL_Window *window = NULL;
-  SDL_Surface *screenSurface = NULL;
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
-    return 1;
-  }
-  window = SDL_CreateWindow("tifftastic", SDL_WINDOWPOS_UNDEFINED,
-                            SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                            SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-  if (window == NULL) {
-    fprintf(stderr, "could not create window: %s\n", SDL_GetError());
-    return 1;
-  }
-  screenSurface = SDL_GetWindowSurface(window);
-  SDL_Rect rect = {0, 0, 1, 1};
-  SDL_FillRect(screenSurface, NULL,
-               SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
-  SDL_UpdateWindowSurface(window);
-  bool quit = false;
-  SDL_Event event;
-  while (!quit) {
-    SDL_WaitEvent(&event);
-    switch (event.type) {
-    case SDL_QUIT:
-      SDL_DestroyWindow(window);
-      SDL_Quit();
-      quit = true;
-    }
-  }
+struct ImageData {
+	int width;
+	int height;
+};
+
+int render(struct ImageData * data) {
+	SDL_Window *window = NULL;
+	SDL_Surface *screenSurface = NULL;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
+		return 1;
+	}
+	window = SDL_CreateWindow("tifftastic", SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED, data->width,
+			data->height, SDL_WINDOW_SHOWN);
+	if (window == NULL) {
+		fprintf(stderr, "could not create window: %s\n", SDL_GetError());
+		return 1;
+	}
+	screenSurface = SDL_GetWindowSurface(window);
+	SDL_Rect rect = {0, 0, 1, 1};
+	SDL_FillRect(screenSurface, NULL,
+			SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
+	SDL_UpdateWindowSurface(window);
+	bool quit = false;
+	SDL_Event event;
+	while (!quit) {
+		SDL_WaitEvent(&event);
+		switch (event.type) {
+			case SDL_QUIT:
+				SDL_DestroyWindow(window);
+				SDL_Quit();
+				quit = true;
+		}
+	}
 }
 
 struct Result read_data() {
-  FILE *fptr;
-  struct TifHeader header;
-  struct TifDirectory directory;
-  struct TifTag tag;
-  // we assume little-endian by default
-  int order = -1;
+	FILE *fptr;
+	struct TifHeader header;
+	struct TifDirectory directory;
+	struct TifTag tag;
+	// we assume little-endian by default
+	int order = -1;
 
-  fptr = fopen("./example.tif", "rb");
-  fread(&header, sizeof(struct TifHeader), 1, fptr);
-  printf("Seeking to %i\n", header.ifd_offset);
-  fseek(fptr, header.ifd_offset, 0);
-  fread(&directory, sizeof(struct TifDirectory), 1, fptr);
+	fptr = fopen("./example.tif", "rb");
+	fread(&header, sizeof(struct TifHeader), 1, fptr);
+	printf("Seeking to %i\n", header.ifd_offset);
+	fseek(fptr, header.ifd_offset, 0);
+	fread(&directory, sizeof(struct TifDirectory), 1, fptr);
 
-  char *data_types[] = {"BYTE", "ASCII", "SHORT", "SHORT", "RATIONAL"};
-  for (int i = 0; i < directory.entry_count; i++) {
-    fread(&tag, sizeof(struct TifTag), 1, fptr);
-    printf("Tag ID: %s\n", get_tag_name(tag.tag_id));
-    printf("Data Type: %s\n", data_types[tag.data_type - 1]);
-    printf("Value count: %i\n", tag.data_count);
-    printf("Data Offset: %i\n", tag.data_offset);
-  }
-  fclose(fptr);
-  return SOME(NULL);
+	char *data_types[] = {"BYTE", "ASCII", "SHORT", "SHORT", "RATIONAL"};
+	struct TifTag *tags = malloc(sizeof(struct TifTag) * directory.entry_count);
+	for (int i = 0; i < directory.entry_count; i++) {
+		fread(&tag, sizeof(struct TifTag), 1, fptr);
+		tags[i] = tag;
+	}
+
+	int width = 0;
+	int height = 0;
+	for (int i = 0; i < directory.entry_count; i++) {
+		if (tags[i].tag_id == ImageWidth) {
+			width = tags[i].data_offset;
+		}
+
+		if (tags[i].tag_id == ImageLength) {
+			height = tags[i].data_offset;
+		}
+
+		printf("\t%s (%i) = %i\n", get_tag_name(tags[i].tag_id), tags[i].tag_id, tags[i].data_offset);
+
+	}
+
+	struct ImageData *data = malloc(sizeof(struct ImageData));
+	data->width = width;
+	data->height = height;
+	fclose(fptr);
+	free(tags);
+	return SOME(data);
 }
 
 int main() {
-  struct Result data = read_data();
-  if (data.is_error) {
-    printf("Could not read tiff file, nested error: %s\n", data.value);
-  }
-  // render();
-  return 0;
+	struct Result result = read_data();
+	if (result.is_error) {
+		printf("Could not read tiff file, nested error: %s\n", result.value);
+	}
+	render(result.value);
+	free(result.value);
+	return 0;
 }
--- tiff.h
+++ tiff.h
@@ -1,34 +1,62 @@
 // Library for interacting with tiff files
 
+enum TagId {
+	ImageWidth = 256,
+	ImageLength = 257,
+	BitsPerSample = 258,
+	Compression = 259,
+	PhotometricInterpretation = 262,
+	ImageDescription = 270,
+	StripOffsets = 273,
+	Orientation = 274,
+	SamplesPerPixel = 277,
+	RowsPerStrip = 278,
+	StripByteCounts = 279,
+	XResolution = 282,
+	YResolution = 283,
+	PlanarConfiguration = 284,
+	ResolutionUnit = 296
+};
+
 char *get_tag_name(int number) {
-  switch (number) {
-  case 256:
-    return "Image Width";
-  case 257:
-    return "Image Length";
-  case 259:
-    return "Compression";
-  case 262:
-    return "Photometric Interpretation";
-  case 273:
-    return "Strip Offsets";
-  case 278:
-    return "Rows Per Strip";
-  case 279:
-    return "Strip Byte Counts";
-  case 282:
-    return "XResolution";
-  case 283:
-    return "YResolution";
-  case 296:
-    return "Resolution Unit";
-  default:
-    return "Unknown";
-  }
+	switch (number) {
+		case ImageWidth:
+			return "Image Width";
+		case ImageLength:
+			return "Image Length";
+		case Compression:
+			return "Compression";
+		case PhotometricInterpretation:
+			return "Photometric Interpretation";
+		case StripOffsets:
+			return "Strip Offsets";
+		case RowsPerStrip:
+			return "Rows Per Strip";
+		case StripByteCounts:
+			return "Strip Byte Counts";
+		case XResolution:
+			return "XResolution";
+		case YResolution:
+			return "YResolution";
+		case ResolutionUnit:
+			return "Resolution Unit";
+		case BitsPerSample:
+			return "Bits Per Sample";
+		case ImageDescription:
+			return "Image Description";
+		case PlanarConfiguration:
+			return "Planar Configuration";
+		case Orientation:
+			return "Orientation";
+		case SamplesPerPixel:
+			return "Samples Per Pixel";
+		default:
+			return "Unknown";
+	}
 }
 
 enum CompressionSchemes {
-  NO_COMPRESSION = 1,
-  MODIFIED_HUFFMAN = 2,
-  PACKBITS = 32773
+	NO_COMPRESSION = 1,
+	MODIFIED_HUFFMAN = 2,
+	PACKBITS = 32773
 };
